e766a3190e88e166eae37b185b6edc3469c49869
gingHUB <jyugal180.jy@gmail.com>
2024-07-01 17:18:21 +0530

    Add and styled the 'Hero' section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -13,10 +13,9 @@ const Section = ({
     <div
       className={`relative ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        }`
-      }`}
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      }
+      ${className || ""}`}
     >
       {children}
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
@@ -29,6 +28,7 @@ const Section = ({
               crossesOffSet && crossesOffSet
             } pointer-events-none lg:block xl:left-10 right-10`}
           />
+          <SectionSvg crossesOffset={crossesOffSet} />
         </>
       )}
     </div>
