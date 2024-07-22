072b3e280287777f3bf343ed7997087300b8e81e
gingHUB <jyugal180.jy@gmail.com>
2024-07-22 20:52:14 +0530

    added new prop 'text'

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,8 @@
-const Heading = ({ classname, title }) => {
+const Heading = ({ classname, title, text }) => {
   return (
     <div className={`${classname} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2>{title}</h2>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 };
