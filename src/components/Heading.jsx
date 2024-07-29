6828fea80af9b23b3a5e86360b10a02bd166bc06
gingHUB <jyugal180.jy@gmail.com>
2024-07-29 22:46:30 +0530

    added and styled price section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,12 @@
-const Heading = ({ classname, title, text }) => {
+import TagLine from "./TagLine";
+
+const Heading = ({ classname, title, text, tag }) => {
   return (
-    <div className={`${classname} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
-      {title && <h2>{title}</h2>}
+    <div
+      className={`${classname} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
+    >
+      {tag && <TagLine className="mb-4 md:justify-center ">{tag}</TagLine>}
+      {title && <h2 className="h2">{title}</h2>}
       {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
